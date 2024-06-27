cd99523157611cc2f0fc1b9315689c2b461c9eba
ethanwithe <fcezaromeroy@gmail.com>
2024-06-26 20:43:49 -0500

    borrado deboug

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Reserva de Restaurante',
+      debugShowCheckedModeBanner: false, 
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
